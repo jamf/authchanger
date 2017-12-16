8e8a577016835ede7f6ffad087d1993ecb6622b5
Joel Rennich <joel@orchardandgrove.com>
2017-12-15 19:24:03 -0600

    Initial Commit

---
+++ authchanger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  authchanger
+//
+//  Created by Joel Rennich on 12/15/17.
+//  Copyright © 2017 Joel Rennich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
