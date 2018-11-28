7a7519cfef6894b44f5a3e6bd6da7a4cc519dbdc
Yohan460 <yoyohan460@gmail.com>
2018-11-28 08:01:03 -0500

    authorizationdb methods file creation

---
+++ authchanger/authorizationdb.swift
@@ -0,0 +1,22 @@
+//
+//  authorizationdb.swift
+//  authchanger
+//
+//  Created by Johan McGwire on 11/28/18.
+//  Copyright © 2018 Orchard & Grove. All rights reserved.
+//
+
+import Foundation
+import Security.AuthorizationDB
+
+func getBatch(getArray: [String]) -> [Dictionary<String,AnyObject>]{
+    return []
+}
+
+func setBatch(setArray: [Dictionary<String,AnyObject>]){
+    
+}
+
+func removeBatch(removeArray: [String]) {
+    
+}
