6c3d4a729fac0c8bd7f60d6bf2e96dee8991ec7c
Yohan460 <yoyohan460@gmail.com>
2018-11-28 08:19:55 -0500

    Function fillout complete in authorizationdb methods

--- authchanger/authorizationdb.swift
+++ authchanger/authorizationdb.swift
@@ -9,14 +9,28 @@
 import Foundation
 import Security.AuthorizationDB
 
-func getBatch(getArray: [String]) -> [Dictionary<String,AnyObject>]{
-    return []
+private func getAuth() -> AuthorizationRef{
+    var authRef : AuthorizationRef? = nil
+    err = AuthorizationCreate(nil, nil, AuthorizationFlags(rawValue: 0), &authRef)
+    return authRef!
 }
 
-func setBatch(setArray: [Dictionary<String,AnyObject>]){
-    
+func getBatch(getArray: [String]) -> Dictionary<String,CFDictionary>{
+    var returnArray: Dictionary<String,CFDictionary> = [:]
+    for getEntry in getArray {
+        err = AuthorizationRightGet(getEntry, &returnArray[getEntry])
+    }
+    return returnArray
+}
+
+func setBatch(setArray: Dictionary<String,Dictionary<String,AnyObject>>){
+    for setEntry in setArray {
+        err = AuthorizationRightSet(getAuth(), setEntry.key, setEntry.value as CFTypeRef, nil, nil, nil)
+    }
 }
 
 func removeBatch(removeArray: [String]) {
-    
+    for removeEntry in removeArray {
+        err = AuthorizationRightRemove(getAuth(), removeEntry)
+    }
 }
